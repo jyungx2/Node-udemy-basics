35d9d93d1fe8cf908faecd0eb023a346a5c51449
jiyoungnim <cloundyon31@gmail.com>
2025-02-23 18:33:27 +0900

    📝 30. Understanding Requests

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const http = require("http"); // global module(without './')
 
 // createServer Callback function: It's called by Node.js whenever a request reaches our server.
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit(); // server 실행을 멈추기 위해 작성해야 하는 코드(=> 웹페이지 동작x, 이 코드 없이 서버는 무한대로 계속해서 실행되는 게 원칙, process.exit()은 말그대로 hard exited our event loop and therefore program shuts down)
+  console.log(req.url, req.method, req.headers); // request 정보 중에 중요한 정보들만 터미널에 출력해보자. (req.url은 url주소('/', '/test', etc...)에 따라 출력값이 달라진다.)
+
+  // process.exit(); // server 실행을 멈추기 위해 작성해야 하는 코드(=> 웹페이지 동작x, 이 코드 없이 서버는 무한대로 계속해서 실행되는 게 원칙, process.exit()은 말그대로 hard exited our event loop and therefore program shuts down)
 });
 
 // listen() is a process where Node.js will not immediately exit our script but where Node.js will instead ✨keep this running to listen for incoming requests.✨
