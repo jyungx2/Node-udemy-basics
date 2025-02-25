c197c9e7a23c2f66f576975192d0b554b87513e5
jiyoungnim <cloundyon31@gmail.com>
2025-02-25 16:30:09 +0900

    📝 68. Adding a 404 Error Page

--- app.js
+++ app.js
@@ -11,4 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// 📌 '/'에 대한 응답처리 미들웨어가 app.get()으로 바뀌면서 라우터 파일 내 지정하지 않은 주소창으로 들어올 때 표시할 페이지 추가할 필요 있음..
+app.use((req, res, next) => {
+  // send()를 부르기 전까지 chain method로 여러 다른 메소드 호출 가능!
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(3000);
