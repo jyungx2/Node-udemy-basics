e639b75a496ad2941ad59a8daab8b73b8c5fc854
jiyoungnim <cloundyon31@gmail.com>
2025-02-25 16:01:04 +0900

    📝 66. Limiting Middleware Execution to POST Requests

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
-app.use("/product", (req, res, next) => {
+// 📌 app.get/post(): Only will fire for incoming get/post request.
+// > get: /product 에 접속하면 실행되는 요청 & 그에 대한 응답
+// > post: add-product로 접속하여 form(method: POST)을 제출하면 실행되는 요청 & 응답 -> 이 경우에만 아래 코드가 실행되며, 그냥 직접적으로 /prdocut로 접속할 때는 실행되지 않고 '/' 주소의 응답처리를 담당하는 미들웨어만 실행.
+app.post("/product", (req, res, next) => {
   console.log(req.body); // undefined -> we need another middleware(=body-parser lib 설치)!
   // body-parser 미들웨어 코드 작성 후, {title: 'Book'}이라고 뜨는 것을 확인 ✅
   res.redirect("/");
